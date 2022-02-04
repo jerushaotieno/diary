134bc1228ae24be38ff71a0f0b88a6388b92566a
jerushaotieno <jerushaotienocoding@gmail.com>
2022-02-04 09:15:18 +0300

    create daily diary entry categories

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Diary';
+  diaryEntry:string[];
+
+  constructor(){
+    this.diaryEntry = ['Daily Positive Affirmation', ' Daily Study', ' Daily Workout']
+  }
 }
